64e1f91890d8d262b06436efd350cd4a679ee684
everblazingjay <everblazingjay@gmail.com>
2023-08-16 00:03:46 +0100

    solved more exercises

--- Day-4/app.js
+++ Day-4/app.js
@@ -43,3 +43,55 @@ if (figure % 2 === 0) {
 
 //Exercise Level 2
 //Q1
+let grade = 90;
+switch (true) {
+  case grade >= 80:
+    console.log("A");
+    break;
+  case grade >= 70 && grade <= 89:
+    console.log("B");
+    break;
+  case grade >= 60 && grade <= 69:
+    console.log("C");
+    break;
+  case grade >= 50 && grade <= 59:
+    console.log("B");
+    break;
+  default:
+    console.log("F");
+}
+
+//Q2
+let month = prompt("Enter the current month");
+if (month === "December" || month === "January" || month === "February") {
+  console.log("The season is winter");
+} else if (month === "March" || month === "April" || month === "May") {
+  console.log("The season is Spring");
+} else if (month === "June" || month === "July" || month === "August") {
+  console.log("The season is summer");
+} else {
+  console.log("The season is autumn");
+}
+
+//Q3
+let today = prompt("Enter the day of the week");
+if (today === "saturday" || today === "sunday") {
+  console.log(`${today} is a weekend`);
+} else {
+  console.log(`${today} is a working day`);
+}
+
+//Exercise level 3
+let theMonth = prompt("Enter the month of your choice");
+if (
+  theMonth === "September" ||
+  theMonth === "April" ||
+  theMonth === "June" ||
+  theMonth === "November"
+) {
+  console.log(`${theMonth} has 30 days`);
+} else if (theMonth === "February") {
+  console.log(`${theMonth} has 28 days, and 29 days in a leap year`);
+} else {
+  console.log(`${theMonth} has 31 days`);
+}
